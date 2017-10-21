65e7570b94c769697c5c24350822dd631b5231c6
Spamntaters <smurra28@uncc.edu>
2017-10-20 23:24:02 -0400

    Done with part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,13 +15,10 @@ class MoviesController < ApplicationController
     @checked_ratings = @all_ratings if !params[:ratings]
     @checked_ratings = params[:ratings].keys if params[:ratings]
     sort_by = params[:sort_by]
- #   session[:ratings] = params[:ratings] if params[:ratings]
     params[:ratings] = session[:ratings] if !params[:ratings]
     @title_hilite = "hilite" if sort_by == "title"
     @release_date_hilite = "hilite" if sort_by == "release_date"
     @movies = Movie.all
-   # @movies = Movie.order(sort_by) if sort_by
-    #@movies = Movie.where(rating: @checked_ratings)
     @movies = Movie.order(sort_by).where(rating: @checked_ratings)
   end
 
